0d4087f34af922de6d010266f01cb3dab1aa91d4
mugabus <fatakimugaruka91@gmail.com >
2024-02-21 12:18:28 +0200

    Basic types

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -1,7 +1,14 @@
 
+pragma solidity ^0.8.23;// SPDX-License-Identifier: MIT
 
-contract SimpleStorage {
+
+
+ contract SimpleStorage {
     //Basic type in solidity...we get:Boolean,uint,int,adress,butes
+    bool hasFavoriteNum =true;
+    uint256 FavoriteNum =99;
+    int256 FavoriteInt = -99;
+    address Myaddress= 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
 
-    
+
 }
